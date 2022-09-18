a88f582118556aadc8a51df929dcff320fbc0899
Dayanand <d.patil717@gmail.com>
2022-09-18 16:39:05 +0530

    Design and functionality of the BMI Calculator in Flutter.

--- lib/bottom_button.dart
+++ lib/bottom_button.dart
@@ -12,9 +12,9 @@ class BottomButton extends StatelessWidget {
     return GestureDetector(
       onTap: onPressed,
       child: Container(
-          color: Color(0xFFEB1555),
-          margin: EdgeInsets.only(top: 10),
-          padding: EdgeInsets.only(bottom: 20),
+          color: const Color(0xFFEB1555),
+          margin: const EdgeInsets.only(top: 10),
+          padding:const EdgeInsets.only(bottom: 20),
           width: double.infinity,
           height: 80,
           child: Center(
